95cfad79bc70cac31a3f591a1e6d4a4cea1a465c
rapha <rapha@141.19.152.224>
2018-10-16 10:41:58 +0200

    Version1.0

---
+++ Test 1/TestdesTestes.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author rapha
+ *
+ */
+public class TestdesTestes {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+         System.out.println("Hello World");
+	}
+
+}
