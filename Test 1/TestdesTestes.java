35f26d0c833e22f72625d111c060bff82f8bfe1d
rapha <rapha@141.19.152.224>
2018-10-16 10:51:01 +0200

    Version1.1

--- Test 1/TestdesTestes.java
+++ Test 1/TestdesTestes.java
@@ -14,7 +14,7 @@ public class TestdesTestes {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-         System.out.println("Hello World");
+         System.out.println("Das denken der Gedanken ist ein gedankenloses Denken also denke nicht gedacht zu haben");
 	}
 
 }
